15e7c8e8ab2afe9110491e0e163906864580a1c3
Mauro_Franco <49313625+Mauro11F@users.noreply.github.com>
2020-04-14 01:44:58 -0300

    punto3
    terminado

--- SQL_consultas_basicas/SQLQuery3.sql
+++ SQL_consultas_basicas/SQLQuery3.sql
@@ -4,5 +4,12 @@ bajo por espacio*/
 use editorial
 go
 
-select nombre, apellido
+select empleado_id as 'empleado id',
+nombre, 
+inicial_segundo_nombre as 'inicial segundo nombre',
+apellido,
+cargo_id as 'cargo id',
+nivel_cargo as 'nivel cargo',
+editorial_id as 'editorial id',
+fecha_contratacion as 'fecha contratacion'
 from empleados
