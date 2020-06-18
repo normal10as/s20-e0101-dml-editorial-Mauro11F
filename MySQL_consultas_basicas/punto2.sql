0851df21435f0420ac391496b98108c47303b60b
Mauro_Franco <49313625+Mauro11F@users.noreply.github.com>
2020-06-18 18:14:01 -0300

    Arreglado
    terminando tps

---
+++ MySQL_consultas_basicas/punto2.sql
@@ -0,0 +1,7 @@
+/*1.2. Listar todas las columnas de empleados y la descripción del cargo que tienen.*/
+
+use editorial;
+
+select empleados.*,cargo_descripcion
+from empleados, cargos
+where empleados.cargo_id = cargos.cargo_id;
---
+++ MySQL_consultas_basicas/punto3.sql
@@ -0,0 +1,14 @@
+/*1.3. Listar la tabla empleados y reemplazar los nombres de columnas que tengan guion
+bajo por espacio*/
+
+use editorial;
+
+select empleado_id as 'empleado id',
+nombre, 
+inicial_segundo_nombre as 'inicial segundo nombre',
+apellido,
+cargo_id as 'cargo id',
+nivel_cargo as 'nivel cargo',
+editorial_id as 'editorial id',
+fecha_contratacion as 'fecha contratacion'
+from empleados;
